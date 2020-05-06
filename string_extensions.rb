c8c78109d5f4ccd098d6596086d801cf2d44e38d
Enow Mbi <benowmbi@yahoo.com>
2020-05-06 20:55:18 +0100

    Add inplace_reversal method

--- string_extensions.rb
+++ string_extensions.rb
@@ -1,11 +1,25 @@
-  class String
-    def palindrome?
-      if self && !self.empty? && self.length > 1
-        return self.reverse == self ? true : false
-      else
-        return false
-      end
+class String
+  def palindrome?
+    if self && !self.empty? && self.length > 1
+      return self.reverse == self ? true : false
+    else
+      return false
     end
   end
+
+  def reverse_inplace!
+    i =0
+    j = self.length - 1
+    while i < j
+      temp = self[i]
+      self[i] = self[j]
+      self[j] = temp
+      i+=1
+      j-=1
+    end
+    return self
+  end
+
+end
 
 
