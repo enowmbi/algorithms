8f972387fe20e0799f7339a7280730aa872d32d1
Enow Mbi <benowmbi@yahoo.com>
2020-05-07 08:52:07 +0100

    Modify sum_closes_to_target to make it cleaner -- searches first for matches then for close matches

--- sum_closest_to_target.rb
+++ sum_closest_to_target.rb
@@ -1,12 +1,14 @@
 def closest_to_target(array1, array2, target)
-closest_matches = []
- array1.each do |a1_el|
+  closest_matches = []
+  array1.each do |a1_el|
     array2.each do |a2_el|
-       if (a1_el + a2_el == target) || (a1_el + a2_el == target + 1) || (a1_el + a2_el == target - 1)
-           closest_matches << [a1_el,a2_el]
-       end
+      if (a1_el + a2_el == target) 
+        closest_matches << [a1_el,a2_el]
+      elsif(a1_el + a2_el == target - 1) || (a1_el + a2_el == target + 1) 
+        closest_matches << [a1_el,a2_el]
+      end
     end
- end
- return closest_matches
+  end
+  return closest_matches
 end
 #solution2 => time complexity = 1 + 1 + n2 => n2 + 2 => O(n2) => quadratic , space => O(n)
