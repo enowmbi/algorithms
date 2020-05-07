19dd021aad5497ae5c9dc28c4ee9c6d68cfd27ac
Enow Mbi <benowmbi@yahoo.com>
2020-05-07 12:08:23 +0100

    Add 'maximum_consecutive_sequence_of_characters' and rename the previous method to 'maximum_number_of_characters'

--- string_extensions.rb
+++ string_extensions.rb
@@ -20,6 +20,51 @@ class String
     return self
   end
 
+  def maximum_number_of_characters
+    max_char =''
+    max_count =0
+    character_hash = {}
+    self.each_char do |char|
+      if character_hash.has_key?(char)
+        character_hash[char] +=1
+      else
+        character_hash[char] = 1
+      end
+      if max_count < character_hash[char]
+        max_char = char
+        max_count = character_hash[char]
+      end
+
+    end
+    return [max_char,max_count]
+  end
+
+  def maximum_consecutive_sequence_of_characters
+    current_char = ''
+    previous_char = ''
+    max_char =''
+    max_count = 0
+    char_stats  ={}
+
+    self.each_char do |char|
+      current_char = char
+      if previous_char == ''
+        char_stats = Hash.new
+        char_stats[char] = 1
+      elsif current_char == previous_char
+        char_stats[char] +=1
+        if max_count < char_stats[char] 
+          max_count = char_stats[char]
+          max_char = char
+        end
+      elsif current_char != previous_char
+        if !char_stats[char]
+          char_stats[char] =  1
+        end
+      end
+      previous_char = current_char
+    end
+    return {max_char.to_sym => max_count}
+  end
+
 end
-
-
