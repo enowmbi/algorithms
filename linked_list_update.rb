d147b0c4d1c26095ea9de1f77fd89dc64af733b6
Enow Mbi <benowmbi@yahoo.com>
2020-06-06 20:32:56 +0100

    Add methods: search, reverse_print and their aliases

--- linked_list_update.rb
+++ linked_list_update.rb
@@ -37,32 +37,68 @@ class LinkedList
   end
 
   def reverse_print(head = @head)
-     current = head
-     if(current == nil)
-       return  current
-     else
-       reverse_print(current.next_node)
-       print "#{current.value} -> "
+    current = head
+    if(current == nil)
+      return  current
+    else
+      reverse_print(current.next_node)
+      print "#{current.value} -> "
+    end
+  end
+
+  def reverse!
+    current = @head
+    next_node = previous = nil
+    while(current != nil)
+      next_node = current.next_node
+      current.next_node = previous
+      previous = current
+      current = next_node
+    end
+    @head = previous
+  end
+
+  def search(value)
+    current = @head
+    results = {} #return node and position of node
+    index = 0
+
+    while(current != nil && index <= @length)
+      if current.value == value
+        results["#{index}"] = current.value 
+        break
+      else
+        current = current.next_node
+      end
+      index +=1
+    end
+    return  results
+  end
+
+ def mid_point
+   middle_point ={}
+   mid = (@length - 1) / 2
+   current = @head
+   index = 0
+   while(current != nil)
+     if (index == mid)
+       middle_point[index] = current.value  
+       break
      end
-  end
+     current = current.next_node
+     index += 1
+   end
+   return middle_point
+ end
 
-def reverse!
-  current = @head
-  next_node = previous = nil
-  while(current != nil)
-    next_node = current.next_node
-    current.next_node = previous
-    previous = current
-    current = next_node
-  end
-  @head = previous
-end
-
-  #TODO get_midpoint,merge_point(other_linked_list),add_another(another_list),insert_at position,remove_at,delete_at,reverse_list, add tail and keep track
+ #TODO get_midpoint,merge_point(other_linked_list),add_another(another_list),insert_at position,remove_at,delete_at, add tail and keep track
 
   alias :size :length
   alias :count :length
   alias :to_s :print_forward
   alias :print_backward :reverse_print
   alias :flip! :reverse!
+  alias :find :search
+  alias :centre :mid_point
+  alias :middle :mid_point
 end
