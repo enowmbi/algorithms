8f53e894ab1c8ad5b940a870d61925be61e98b98
Enow Mbi <benowmbi@yahoo.com>
2020-06-11 12:04:59 +0100

    Started backspace string compare

--- backspace_string_compare.rb
+++ backspace_string_compare.rb
@@ -1,23 +1,54 @@
-def backspace_string_compare(string1,string2)
- while(string1.include?'#') 
-   0.upto(string1.length - 1) do |i|
-      if string1[i] == '#'
-        string1.sub!(string1[i],'')
-        string1.sub!(string1[i - 1],'') if i > 1
-      end
-   end
- end
+def backspace_string_compare(s,t)
+  s_pointer = s.length - 1
+  t_pointer = t.length - 1
 
- while(string2.include?'#') 
-   0.upto(string2.length - 1) do |i|
-      if string2[i] == '#'
-        string2.sub!(string2[i],'')
-        string2.sub!(string2[i - 1],'') if i > 1
-      end
-   end
- end
+  s_skips = 0
+  t_skips = 0
 
- return string1 == string2 ? true : false
+  while((s_pointer >= 0) || (t_pointer >= 0))
+    while(s_pointer <= 0)
+      if s[s_pointer] == '#'
+        # s.sub!('#','')
+        s_skips += 1
+        s_pointer -= 1
+      else
+        if s_skips > 0 
+          # s.sub!(s[s_pointer],'')
+          s_skips -= 1
+          s_pointer -= 1
+        else
+          break
+        end
+      end
+    end
+
+    while(t_pointer <= 0)
+      if t[t_pointer] == '#'
+        # t.sub!('#','')
+        t_skips += 1
+        t_pointer -= 1
+      else
+        if t_skips > 0 
+          # t.sub!(s[t_pointer],'')
+          t_skips -= 1
+          t_pointer -= 1
+        else
+          break
+        end
+      end
+    end
+
+    if(s_pointer >= 0 && t_pointer >= 0 && s[s_pointer] != t[t_pointer])
+      return false
+    end
+    if((s_pointer >= 0) != (t_pointer >= 0))
+      return false
+    end
+
+    s_pointer -= 1
+    t_pointer -= 1
+  end 
+  return true 
 end
 
 
