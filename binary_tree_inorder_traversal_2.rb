90f943e282b9beab6bdaf6f155062b26764c69f8
Enow Mbi <benowmbi@yahoo.com>
2020-06-18 15:33:14 +0100

    Solve binary tree inorder traversal using iteration

---
+++ binary_tree_inorder_traversal_2.rb
@@ -0,0 +1,22 @@
+def inorder_traversal(root)
+  #left,node,right   => we'll use the iterative technique
+  return root if root == nil
+  stack_of_nodes = []
+  results =[]
+
+  current = root
+  while(current != nil || !stack_of_nodes.empty?)
+
+    while(current != nil)
+      stack_of_nodes << current
+      current = current.left
+    end
+
+    last_node = stack_of_nodes.pop
+    results << last_node.val
+    current = current.right
+  end
+
+return results
+
+end
