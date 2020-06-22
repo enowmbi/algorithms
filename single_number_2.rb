651c150beb8caafafff8ad142af2c22beea41ed7
Enow Mbi <benowmbi@yahoo.com>
2020-06-22 09:41:02 +0100

    Solve single number in an array using parity --add comments

--- single_number_2.rb
+++ single_number_2.rb
@@ -7,3 +7,6 @@ def single_number(nums)
   return result
 
 end
+ #result ^= num = num if result is not number
+#result ^= num = 0 if result is number 
+#so if the number is there already you'll have 0 otherwise have number so the first non-zero is the single number
