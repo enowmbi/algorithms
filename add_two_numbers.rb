16e4d3fce42b74c1e530a39a211c848e7c143d88
Enow Mbi <benowmbi@yahoo.com>
2020-06-25 11:06:08 +0100

    Solve add two numbers using linked list

--- add_two_numbers.rb
+++ add_two_numbers.rb
@@ -1,4 +1,34 @@
-def add_two_numbers(node1,node2)
+def add_two_numbers(list_node_1,list_node_2)
+  pointer_1 = list_node_1
+  pointer_2 = list_node_2
+  list_node_3 = ListNode.new()
+  pointer_3 = list_node_3
+  carry_over = 0
 
+  while(pointer_1 != nil || pointer_2 != nil)
+    val1 = pointer_1 ? pointer_1.val : 0
+    val2 = pointer_2 ? pointer_2.val : 0
+    
+    current_sum = val1 + val2 + carry_over
+last_digit = current_sum % 10
+carry_over = current_sum / 10
+
+    new_node = ListNode.new(last_digit)
+    pointer_3.next = new_node
+
+    pointer_1 = pointer_1.next if pointer_1
+    pointer_2 = pointer_2.next if pointer_2
+    pointer_3 = pointer_3.next
+  end
+
+  if(carry_over > 0)
+     new_node = ListNode.new(carry_over)
+     pointer_3.next = new_node
+     pointer_3 = pointer_3.next
+  end
+ 
+  return l3.next
 
 end
+
+#numbers are stored in reverse form so no need to reverse them
