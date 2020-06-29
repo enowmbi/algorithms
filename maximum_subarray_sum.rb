5d746c1e60f4077addb9e6247353355e06f109bb
Enow Mbi <benowmbi@yahoo.com>
2020-06-29 05:13:59 +0100

    Solve maximum subarray sum -- add conditions for all positive numbers and all negative numbers

--- maximum_subarray_sum.rb
+++ maximum_subarray_sum.rb
@@ -1,4 +1,6 @@
 def max_subarray_sum(arr)
+  return arr.max if arr.max < 0 # if array has only negative numbers
+  return arr.sum if arr.min > 0 # returns the sum of the array items if all elements are positive integers
   current_sum = max_sum = 0  #assumming there is atleast one positive integer
   0.upto(arr.length - 1) do |i|
     current_sum += arr[i]
@@ -15,6 +17,8 @@ end
 #runs in O(n) time complexity and O(n) space 
 
 def max_subarray_sum2(arr)
+  return arr.max if arr.max < 0 # if array has only negative numbers
+  return arr.sum if arr.min > 0 # returns the sum of the array items if all elements are positive integers
   current_sum = max_sum = 0 
   0.upto(arr.length - 1) do |i|
     if(current_sum + arr[i] > 0)
