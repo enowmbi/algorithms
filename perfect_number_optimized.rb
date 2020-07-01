57b440d7b1b4c9b879a13b4aa25fc076809defb7
Enow Mbi <benowmbi@yahoo.com>
2020-07-01 12:13:57 +0100

    Solve perfect number -- optimized solution

---
+++ perfect_number_optimized.rb
@@ -0,0 +1,18 @@
+i# @param {Integer} num
+# @return {Boolean}
+def check_perfect_number(num)
+  return false if num <= 1
+  positive_divisors = [1] #initialize positive divisors with 1
+  max_factor = Math.sqrt(num).to_i
+  2.upto(max_factor) do |divisor|
+    if(num % divisor == 0 && num / divisor < num)
+      positive_divisors << divisor
+      positive_divisors << num / divisor
+    end
+  end
+
+  return positive_divisors.sum == num ? true : false
+
+end
+
+
