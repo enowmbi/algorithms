2b14d1e82c6846c8c9cb61faf920c92b2c56c4d7
Enow Mbi <benowmbi@yahoo.com>
2020-07-03 14:17:11 +0100

    Solve binary tree postorder traversal

---
+++ binary_tree_postorder_traversal.rb
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {Integer[]}
+def postorder_traversal(root)
+  result = []
+  return result if root == nil
+  stack = []
+  stack << root
+  while(!stack.empty?)
+    current_node = stack.pop
+    result.insert(0,current_node.val)  # or call reverse at the end
+    if(current_node.left != nil)
+      stack << current_node.left
+    end
+    if(current_node.right != nil)
+      stack << current_node.right
+    end
+  end
+  return result
+end
