d9ee04f33324075595b86dbf878f6ff5d719213d
Enow Mbi <benowmbi@yahoo.com>
2020-07-03 15:38:14 +0100

    Modify solution to longest common prefix problem

--- longest_common_prefix.rb
+++ longest_common_prefix.rb
@@ -1,17 +1,16 @@
-def longest_common_prefix(words =[])
-  return "" if words.empty?
-
-  prefix = words.first
-
-  1.upto(words.length - 1) do |i| 
-    while(!words[i].start_with?(prefix))
-      prefix.delete!(prefix.chars.last)
+# @param {String[]} strs
+# @return {String}
+def longest_common_prefix(strs)
+  return "" if strs.empty?
+  prefix = strs.first
+  1.upto(strs.length - 1) do |i|
+    while(!strs[i].start_with?prefix)
+      prefix[prefix.length - 1] = ''
     end
   end
-
-  return prefix
-
+    return prefix
 end
 
 #given an array of words,return the longest common prefix for all words  or "" if there is no common prefix
 #procedure : get the first word from the array as potential_prefix  as we iterate through the words we'll get the confirmed prefix which we'll return 
+#we could also sort by length of words and pick the first word which will be the shortest word and use it as our initial prefix and reduce the word from the end as we iterate
