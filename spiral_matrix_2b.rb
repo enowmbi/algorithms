97b371b8cf78af9723d922258025d5c38b810f5e
Enow Mbi <benowmbi@yahoo.com>
2020-07-08 10:04:20 +0100

    Solution to spiral matrix 2 using array of array

---
+++ spiral_matrix_2b.rb
@@ -0,0 +1,45 @@
+# @param {Integer} n
+# @return {Integer[][]}
+def generate_matrix(n)
+  return [] if n == 0
+  result = Array.new(n){Array.new(n,0)}    
+  row_start = column_start = 0
+  row_end = column_end = n - 1 
+  count = 1
+  direction = 0
+  while(row_start <= row_end && column_start <= column_end)
+    if direction  == 0
+      column_start.upto(column_end) do |column_idx|
+        result[row_start][column_idx] = count
+        count += 1
+      end
+      row_start += 1
+      direction += 1
+
+    elsif direction == 1
+      row_start.upto(row_end) do |row_idx|
+        result[row_idx][column_end] = count
+        count += 1
+      end
+      column_end -= 1
+      direction += 1
+
+    elsif direction == 2
+      column_end.downto(column_start) do |column_idx|
+        result[row_end][column_idx] = count
+        count += 1
+      end
+      row_end -= 1
+      direction += 1
+
+    elsif direction == 3
+      row_end.downto(row_start) do |row_idx|
+        result[row_idx][column_start] = count
+        count += 1
+      end            
+      column_start += 1
+      direction  = 0
+    end       
+  end
+  return result 
+end
