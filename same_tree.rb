e4246995d63ba63e202330a3a2d3e5ad789eb12a
Enow Mbi <benowmbi@yahoo.com>
2020-07-10 11:32:53 +0100

    Solution to same tree -- using recursion

---
+++ same_tree.rb
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} p
+# @param {TreeNode} q
+# @return {Boolean}
+
+def is_same_tree(p, q)
+	return true if p == nil && q == nil
+	return false if p == nil || q == nil
+	return p.val == q.val && is_same_tree(p.left,q.left) && is_same_tree(p.right,q.right) 
+end
+
