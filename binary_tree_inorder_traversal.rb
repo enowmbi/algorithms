48b65a50639d2e47f970349b3f037db529d753cb
Enow Mbi <benowmbi@yahoo.com>
2020-07-13 06:36:57 +0100

    Solution to binary tree inorder traversal

--- binary_tree_inorder_traversal.rb
+++ binary_tree_inorder_traversal.rb
@@ -1,14 +1,16 @@
 def inorder_traversal(root)
-  #returns and array of the values 
   result = []
-  if root == nil
-    result << root
-    return result
-  else
-    inorder_traversal(root.left)
-    result << root.value
-    inorder_traversal(root.right)
+  return result if root == nil
+  stack = [] 
+  current_node = root
+  while(stack.empty? || current_node != nil)
+    while(current_node != nil)
+      stack << current_node
+      current_node = current_node.left
+    end
+    current_node = stack.pop
+    result << current_node.val
+    current_node = current_node.right 
   end
-  return result
 end
 
