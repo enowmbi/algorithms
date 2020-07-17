bad910305743e30bc5fd4a0fb10764f914089d84
Enow Mbi <benowmbi@yahoo.com>
2020-07-17 18:10:40 +0100

    Update merge sort

--- merge_sort.rb
+++ merge_sort.rb
@@ -3,19 +3,19 @@ def merge_sort(arr1,arr2)
   i = 0
   j = 0
   while(i < arr1.length  && j < arr2.length)
-     if arr1[i] < arr2[j]
-       arr3 << arr1[i]
-       i += 1
-     elsif arr1[i] > arr2[j]
-       arr3 << arr2[j]
-       j += 1
-     end
+    if arr1[i] <= arr2[j]
+      arr3 << arr1[i]
+      i += 1
+    elsif arr1[i] > arr2[j]
+      arr3 << arr2[j]
+      j += 1
+    end
   end
 
-    while(i < arr1.length)
-     arr3 << arr1[i]
-     i += 1
-    end
+  while(i < arr1.length)
+    arr3 << arr1[i]
+    i += 1
+  end
 
   while(j < arr2.length)
     arr3 << arr2[j]
