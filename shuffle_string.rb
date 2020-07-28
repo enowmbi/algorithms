9443a7423ab8bdc2fcd7dbfc940d4db45e3bf2d6
Enow Mbi <benowmbi@yahoo.com>
2020-07-28 13:07:54 +0100

    Solution to shuffle string

---
+++ shuffle_string.rb
@@ -0,0 +1,14 @@
+# @param {String} s
+# @param {Integer[]} indices
+# @return {String}
+def restore_string(s, indices)
+  char_hash = {}
+  position = 0
+  #creat a hash that uses position as key and the chars as values
+  indices.each do |index|        
+    char_hash[index] = s[position]
+    position += 1
+  end
+  indices = char_hash.keys.sort.map{ |key| char_hash[key]}.join
+
+end
