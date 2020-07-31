0d8a369f78f8581c3d74e0f70464ba4a0e129f9e
Enow Mbi <benowmbi@yahoo.com>
2020-07-31 11:47:58 +0100

    Solve max length of consecutive squence in array

---
+++ byte_by_byte/consecutive_array.rb
@@ -0,0 +1,20 @@
+#Question 5 -- find length of max consecutive sequence in an array
+#iterate through the array, if element's next value in sequence exist, add it exist
+#we'll try to solve it in O(n) time -- linear time
+
+def max_consecutive_array_length(arr)
+  max_length = 0
+  return max_length if arr == nil || arr.empty?
+
+  arr.each do |num|
+    current_length = 0
+    while(arr.include?(num.next))
+      current_length += 1
+      num = num.next
+      if current_length > max_length 
+        max_length = current_length
+      end
+    end
+  end
+  return max_length + 1
+end
