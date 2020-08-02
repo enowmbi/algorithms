f001bfb32e668e886a01ee76198141918f7ca0e9
Enow Mbi <benowmbi@yahoo.com>
2020-08-02 14:48:42 +0100

    Design hashset solution

---
+++ byte_by_byte/find_duplicates_2.rb
@@ -0,0 +1,3 @@
+#question 2: find duplicates -- assumming that the order matters
+#We'll solve this challenge using O(nlogn) time and constant space-O(1)
+#
---
+++ design_hashset.rb
@@ -0,0 +1,47 @@
+class MyHashSet
+
+=begin
+    Initialize your data structure here.
+=end
+  attr_accessor :container
+
+  def initialize()
+    @container = Array.new()
+  end
+
+
+=begin
+    :type key: Integer
+    :rtype: Void
+=end
+  def add(key)
+    @container[key] =true
+  end
+
+
+=begin
+    :type key: Integer
+    :rtype: Void
+=end
+  def remove(key)
+    @container[key] = nil
+  end
+
+
+=begin
+    Returns true if this set contains the specified element
+    :type key: Integer
+    :rtype: Boolean
+=end
+  def contains(key)
+    !!@container[key]
+  end
+
+
+end
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet.new()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
